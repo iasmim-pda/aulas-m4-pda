bcaa1e05bbe55be043ad0bb369a550ace2760d26
Iasmim Santos <iasmim.santos@grupoboticario.com.br>
2024-07-01 10:36:23 -0300

    aulas

---
+++ a2/script.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+//Estou colocando dentro da constante o modulo chalk que eu instalei
+console.log(chalk.cyan("Sou um texto ciano"));
+console.log(chalk.red("Sou um texto vermelho"));
+console.log(chalk.yellow("Sou um texto amarelo"));
+
